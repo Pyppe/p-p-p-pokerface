b26bc3a1b92a47932a7366307586688a5c0cce81
Pyry-Samuli Lahti <pyppe@iki.fi>
2015-09-06 13:21:04 +0300

    Yeehaw!

--- src/p_p_p_pokerface.clj
+++ src/p_p_p_pokerface.clj
@@ -1,34 +1,77 @@
 (ns p-p-p-pokerface)
 
+; http://iloveponies.github.io/120-hour-epic-sax-marathon/p-p-p-pokerface.html
+
 (defn rank [card]
-  nil)
+  (let [[r _] card
+        replacements {\T 10
+                      \J 11
+                      \Q 12
+                      \K 13
+                      \A 14}]
+    (if (Character/isDigit r)
+      (Integer/valueOf (str r))
+      (get replacements r)
+      )))
 
 (defn suit [card]
-  nil)
+  (let [[_ suit] card]
+    (str suit)))
+
+(defn rank-frequencies [hand]
+  (let [ranks (map rank hand)]
+    (frequencies ranks)))
+
+(defn suit-frequencies [hand]
+  (let [suits (map suit hand)]
+    (frequencies suits)))
+
+(defn contains-of-n-kind [hand n]
+  (contains? (set (vals (rank-frequencies hand))) n))
 
 (defn pair? [hand]
-  nil)
+  (contains-of-n-kind hand 2))
 
 (defn three-of-a-kind? [hand]
-  nil)
+  (contains-of-n-kind hand 3))
 
 (defn four-of-a-kind? [hand]
-  nil)
+  (contains-of-n-kind hand 4))
 
 (defn flush? [hand]
-  nil)
+  (== (count (set (vals (suit-frequencies hand)))) 1))
 
 (defn full-house? [hand]
-  nil)
+  (and (pair? hand)
+       (three-of-a-kind? hand)))
 
 (defn two-pairs? [hand]
-  nil)
+  (= 2 (get (frequencies (vals (rank-frequencies hand))) 2)))
 
+; hhngh.. ugleeeh!
 (defn straight? [hand]
-  nil)
+  (let [ranks (map rank hand)
+        is-numeric-straight?
+          (fn [sorted-ranks]
+            (and
+              (apply < sorted-ranks)
+              (== (- (apply max sorted-ranks) (apply min sorted-ranks)) 4)))]
+    (or
+      (is-numeric-straight? (sort ranks))
+      (is-numeric-straight? (sort (replace {14 1} ranks))))))
 
 (defn straight-flush? [hand]
-  nil)
+  (and (flush? hand)
+       (straight? hand)))
 
 (defn value [hand]
-  nil)
+  (cond
+    (straight-flush?  hand) 8
+    (four-of-a-kind? hand)  7
+    (full-house? hand)      6
+    (flush? hand)           5
+    (straight? hand)        4
+    (three-of-a-kind? hand) 3
+    (two-pairs? hand)       2
+    (pair? hand)            1
+    :else                   0))
